8af821be0a2d4943602c8316434f1ee0c8aadcb5
Tomer_Kulish <91198141+tomerkul@users.noreply.github.com>
2023-07-04 15:54:39 +0300

    Update findlastversion.py

--- sample-flask/application/findlastversion.py
+++ sample-flask/application/findlastversion.py
@@ -18,6 +18,14 @@ image_name = f"tomerkul/myflask:{next_version}"
 client.images.build(path=".", tag=image_name, rm=True, pull=True)
 print(f"Successfully built image: {image_name}")
 
-# Push the image to Docker Hub
+# Push the image with the specified tag
 client.images.push(repository="tomerkul/myflask", tag=next_version)
 print(f"Successfully pushed image: {image_name}")
+
+# Tag the image as 'latest' and push it
+latest_tag = "latest"
+latest_image_name = f"tomerkul/myflask:{latest_tag}"
+client.images.tag(image_name, repository="tomerkul/myflask", tag=latest_tag)
+client.images.push(repository="tomerkul/myflask", tag=latest_tag)
+print(f"Successfully pushed image: {latest_image_name}")
+
